6c5b209fd9fad7535dd88c652b6a9c2e19b53798
guyt-ds <58878443+guyt-ds@users.noreply.github.com>
2024-10-24 10:41:34 +0300

    Update basicExample.js

--- examples/basicExample.js
+++ examples/basicExample.js
@@ -1,8 +1,8 @@
 import { trackUserAction } from 'affiliate-ton-library';
 
-const contractAddress = 'your_campaign_contract_address';  // Each Campaign is a smart contract on TON.  Extract address from your campaign page. 
-const userActionOpCode = 2001; // op code for this operation (defined in your Campaign as a customized event.  Each such event has a unique OP Code, e.g. 2001 for user registration)
-const affiliateId = 12; // extract from URL or cookies (depending on your affiliate link construct)
+const contractAddress = 'your_campaign_contract_address';  // Each Campaign is a smart contract on TON.  Extract address from your Campaign page. 
+const userActionOpCode = 2001; // Each user action has a unique op code.  Extract op code from your Campaign page (e.g. 2001 for user registration, 2002 for purchase, etc...)
+const affiliateId = 12; // extract from URL or cookies (depending on your affiliate link construct).
 
 document.getElementById('registerButton').addEventListener('click', async () => {
     await trackUserAction(contractAddress, userActionOpCode, affiliateId);
